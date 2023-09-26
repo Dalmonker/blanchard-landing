058c1152110992657c7d380bba719a56515c1bba
Dalmonker <gavrilovichdmitry1@gmail.com>
2023-09-26 17:59:16 +0300

    base-url

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import autoprefixer from "autoprefixer"
-import { defineConfig } from "vite"
+import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   css: {
     preprocessorOptions: {},
     postcss: {
-      plugins: [autoprefixer({})]
-    }
-  }
-})
+      plugins: [autoprefixer({})],
+    },
+  },
+  base: "https://dalmonker.github.io/blanchard-landing/",
+});
